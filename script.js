e87988bb4527554ef2c43bdbaa0ab010fa46a347
arjunbirsingh <110299503+arjunbirsingh@users.noreply.github.com>
2023-10-01 19:38:55 +0530

    cont4 done

--- script.js
+++ script.js
@@ -6,23 +6,25 @@ window.addEventListener("load", () => {
 const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
-  mobile:{
-    smooth:true
-  }
+  mobile: {
+    smooth: true,
+  },
 });
 let body = document.body;
 let playbtn = document.querySelector(".cont1 .play");
 let video = document.querySelector(".cont1 video");
 let videoCont = document.querySelector(".videoCont");
-function playbtnMove(dets){{
-  if (window.innerWidth < 1400) {
-  } else {
-    playbtn.style.position = "fixed";
-    playbtn.style.left = dets.x + "px";
-    playbtn.style.top = dets.y + "px";
-    playbtn.style.transform = `scale(1) translate(-50%,-50%) `;
+function playbtnMove(dets) {
+  {
+    if (window.innerWidth < 1400) {
+    } else {
+      playbtn.style.position = "fixed";
+      playbtn.style.left = dets.x + "px";
+      playbtn.style.top = dets.y + "px";
+      playbtn.style.transform = `scale(1) translate(-50%,-50%) `;
+    }
   }
-}}
+}
 function playbtnAll() {
   videoCont.addEventListener("mouseover", playbtnMove);
   videoCont.addEventListener("mousemove", playbtnMove);
@@ -59,4 +61,23 @@ gsap.from(".videoCont .play", {
   y: "50%",
   duration: 1,
   ease: Expo,
+});
+let cont4 = document.querySelector(".cont4");
+let cont4Items = document.querySelectorAll(".cont4 .item img");
+let hoverDiv = document.querySelector(".cont4 .hoverDiv");
+function hoverDivScaleUp(dets) {
+  hoverDiv.style.transform = `scale(1) translate(-50%,-50%)`;
+  hoverDiv.style.top = dets.y + "px";
+  hoverDiv.style.left = dets.x + "px";
+  hoverDiv.style.opacity = 1;
+}
+function hoverDivScaleDown() {
+  hoverDiv.style.transform = `scale(0)`;
+  hoverDiv.style.opacity = 0;
+}
+cont4Items.forEach((element) => {
+  element.addEventListener("mousemove", hoverDivScaleUp);
+});
+cont4Items.forEach((element) => {
+  element.addEventListener("mouseleave", hoverDivScaleDown);
 });
