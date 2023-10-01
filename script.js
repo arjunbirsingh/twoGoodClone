136eecfe44ce52205618e4a005c0f118198b0406
arjunbirsingh <110299503+arjunbirsingh@users.noreply.github.com>
2023-10-01 20:20:47 +0530

    almost done

--- script.js
+++ script.js
@@ -10,6 +10,43 @@ const scroll = new LocomotiveScroll({
     smooth: true,
   },
 });
+function locoPlusScrollTrigger(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+locoPlusScrollTrigger();
 let body = document.body;
 let playbtn = document.querySelector(".cont1 .play");
 let video = document.querySelector(".cont1 video");
@@ -60,6 +97,18 @@ gsap.from(".videoCont .play", {
   opacity: 0,
   y: "50%",
   duration: 1,
+  ease: Expo,
+});
+gsap.from(".cont2 .floatingMenu", {
+  opacity: 0,
+  scrollTrigger: {
+    scroller: "#main",
+    start: "top center",
+    yoyo:"true",
+    end: "bottom center",
+    scrub: 2,
+  },
+  duration: 2,
   ease: Expo,
 });
 let cont4 = document.querySelector(".cont4");
